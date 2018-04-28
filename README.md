eab747e7947ec339cf9ab201bc2008fe8b17afd7
Hundemeier <hundemeier99@gmail.com>
2018-04-28 14:48:33 +0200

    reformatted the receiver part

--- README.md
+++ README.md
@@ -12,10 +12,16 @@ If you want to see a full DMX package, see the
 This is currently the only implemented feature. The simplest way to receive sACN packets is 
 to use `receiver.Receiver`.
 
-`Receiver` takes two arguments: a channel for `packets.DataPacket`s and the universe on 
-which the function should listen on. The channel returns every packet that is received on the given 
-universe. If a timeout occurred (2,5s no message) or a packet with the StreamTermination-bit was set, 
-the channel will close.
+`Receiver` takes the universe on which the function should listen on and an interface name to bind to.
+If you want to listen on all interfaces use an empty string "". 
+It returns a data channel and an error channel.
+The data channel returns every packet that is received on the given universe. If a timeout occurred 
+(2,5s no message) or a packet with the StreamTermination-bit was set, the channel will close.
+
+Please note that the data is not sorted by seqeunce number nor by priority. So these two parameters
+must be handled in your program. See pages 12 and 17 in [E1.31][e1.31] for more information.
+Synchronization must also be implemented in your program, but currently there is no way to receive
+the sACN sync-packets. This feature may come in a future version.
 
 Please note: This implementation is subjected to change!
 
@@ -30,12 +36,16 @@ import (
 )
 
 func main() {
-	ch := make(chan sacn.DataPacket)
-	go sacn.Receive(ch, 1)
-
+	//listen on universe 1 and bind to all interfaces
+	ch, _ := sacn.Receive(1, "") //returns a data channel and an error channel
+	//in this example we dont wnat to mess with errors
 	for i := range ch {
+		//print as long as we receive data
 		fmt.Println(i.Data())
 	}
+	//now the channel was closed and the program exits
+	//the channel was closed, because the universe timeouted and no more data was received
+	//if we want we cloud start listening again. Depends on the use case
 }
 ```
 
--- sacn/receiver.go
+++ sacn/receiver.go
@@ -5,40 +5,56 @@ import (
 	"time"
 )
 
-//Receive uses a channel to return the sACN packets that are received on ":5568"
-func Receive(c chan<- DataPacket, universe uint16) error {
-	ServerAddr, err := net.ResolveUDPAddr("udp", ":5568")
-	if err != nil {
-		return err
-	}
+//Receive returns two chnnels: one for data and one for errors.
+//the data channel only returns data from the universe that was given.
+//parameters: universe: universe to listen on; bind: the interface on which the listener should bind to
+func Receive(universe uint16, bind string) (<-chan DataPacket, <-chan error) {
+	data := make(chan DataPacket)
+	errch := make(chan error)
 
-	ServerConn, err := net.ListenUDP("udp", ServerAddr)
-	if err != nil {
-		return err
-	}
-	defer ServerConn.Close()
-
-	buf := make([]byte, 638)
-	lastTime := time.Now()
-	for {
-		//Set the timout according to the E1.31 protocol
-		ServerConn.SetDeadline(time.Now().Add(time.Millisecond * 2500))
-		n, addr, _ := ServerConn.ReadFromUDP(buf) //n, addr, err
-		if addr == nil {                          //Check if we had a timeout
-			close(c) //Close the channel in case of a timeout
-		}
-		p, err := NewDataPacketRaw(buf[0:n])
+	//Receive the unprocessed data and sort out the ones with the corrct universe
+	go func() {
+		ServerAddr, err := net.ResolveUDPAddr("udp", bind+":5568")
 		if err != nil {
-			break
+			errch <- err
 		}
-		if p.Universe() == universe {
-			lastTime = time.Now()
-			c <- p
-		} else if time.Since(lastTime) >= time.Millisecond*2500 ||
-			p.StreamTerminated() {
-			close(c) //If we are here we had a atimeout on the universe
-			//or a strem termination
+
+		ServerConn, err := net.ListenUDP("udp", ServerAddr)
+		if err != nil {
+			errch <- err
 		}
-	}
-	return nil
+		defer ServerConn.Close()
+
+		buf := make([]byte, 638)
+		//store the lasttime a packet on the universe was received
+		lastTime := time.Now()
+		for {
+			//Set the timout according to the E1.31 protocol
+			ServerConn.SetDeadline(time.Now().Add(time.Millisecond * 2500))
+			n, addr, _ := ServerConn.ReadFromUDP(buf) //n, addr, err
+			if addr == nil {                          //Check if we had a timeout
+				break //escape the for loop
+			}
+			p, err := NewDataPacketRaw(buf[0:n])
+			if err != nil {
+				errch <- err
+			}
+			if p.Universe() == universe {
+				//received a packet on the universe to listen on
+				lastTime = time.Now()
+				data <- p
+
+				if p.StreamTerminated() {
+					//if the stream termination bit was set, we escape the loop to close the channel
+					break
+				}
+			} else if time.Since(lastTime) > 2500*time.Millisecond {
+				break
+			}
+		}
+		close(errch)
+		close(data)
+	}()
+
+	return data, errch
 }
